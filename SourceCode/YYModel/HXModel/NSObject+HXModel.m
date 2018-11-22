949bbb8dfaef4ccb78810266f3f49bf423fd5e29
Insofan <282132315@qq.com>
2018-11-22 11:06:07 +0800

    yymodel nsobject+hxmodel 完成

---
+++ SourceCode/YYModel/HXModel/NSObject+HXModel.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+HXModel.m
+//  SourceCode
+//
+//  Created by Insomnia on 2018/11/22.
+//  Copyright © 2018 Insomnia. All rights reserved.
+//
+
+#import "NSObject+HXModel.h"
+
+@implementation NSObject(HXModel)
+
+@end
